c1b9c5b26eaec137c39d7f849fa8014d88ddc794
Ivan Shikht <ishikht@exoft.net>
2023-02-24 17:58:35 +0200

    Added integration tests

---
+++ tests/IntegrationTests/InvestmentTracking.Api.IntegrationTests/BrokersControllerTests.cs
@@ -0,0 +1,42 @@
+﻿using InvestmentTracking.Core.Dtos;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvestmentTracking.Api.IntegrationTests
+{
+    public class BrokersControllerTests : IClassFixture<TestFixture>
+    {
+        private readonly TestFixture _fixture;
+        private readonly HttpClient _client;
+
+        public BrokersControllerTests(TestFixture fixture)
+        {
+            _fixture = fixture;
+            _client = fixture.Client;
+        }
+
+        [Fact]
+        public async Task CreateBroker_ReturnsCreatedBroker()
+        {
+            // Arrange
+            var broker = new BrokerDto
+            {
+                Name = "Test Broker",
+            };
+            var content = new StringContent(JsonConvert.SerializeObject(broker), Encoding.UTF8, "application/json");
+
+            // Act
+            var response = await _client.PostAsync("/brokers", content);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            var responseContent = await response.Content.ReadAsAsync<BrokerDto>();
+            Assert.Equal(broker.Name, responseContent.Name);
+            Assert.NotEqual(Guid.Empty, responseContent.Id);
+        }
+    }
+}
---
+++ tests/IntegrationTests/InvestmentTracking.Api.IntegrationTests/TestFixture.cs
@@ -0,0 +1,88 @@
+﻿using Ductus.FluentDocker.Builders;
+using Ductus.FluentDocker.Commands;
+using Ductus.FluentDocker.Services;
+
+namespace InvestmentTracking.Api.IntegrationTests
+{
+    public class TestFixture : IDisposable
+    {
+        private readonly string _dockerComposeFilePath;
+        private readonly int _dockerComposeStartupTimeoutSeconds;
+        private ICompositeService _dockerComposeService;
+
+        public HttpClient Client { get; }
+
+        public TestFixture()
+        {
+            // Define the path to the Docker Compose file
+            _dockerComposeFilePath = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName, "docker-compose.yml");
+
+            // Define the Docker Compose startup timeout in seconds
+            _dockerComposeStartupTimeoutSeconds = 60;
+
+            // Create an HTTP client to use in the tests
+            Client = new HttpClient { BaseAddress = new Uri("http://localhost:5000") };
+
+            // Start the Docker Compose configuration
+            StartDockerCompose();
+        }
+
+        public void Dispose()
+        {
+            // Stop and remove the Docker Compose configuration
+            _dockerComposeService.Stop();
+            _dockerComposeService.Dispose();
+        }
+
+        private void StartDockerCompose()
+        {
+            // Build and start the Docker Compose configuration
+            _dockerComposeService = new Builder()
+                .UseContainer()
+                .UseCompose()
+                .FromFile(_dockerComposeFilePath)
+                .ServiceName("investment-tracking-integration_tests")
+                .WaitForPort("api", "80", 30000)
+                .Build()
+                .Start();
+
+            // Wait for the containers to start up
+            WaitForDockerComposeStartup();
+        }
+
+        private async void WaitForDockerComposeStartup()
+        {
+            // Define the timeout
+            var timeout = TimeSpan.FromSeconds(_dockerComposeStartupTimeoutSeconds);
+
+            // Define the delay between checks
+            var delay = TimeSpan.FromSeconds(5);
+
+            // Define the start time
+            var startTime = DateTime.Now;
+
+            // Check the status of the containers
+            while (DateTime.Now - startTime < timeout)
+            {
+                var apiContainer = _dockerComposeService.Containers.First(c => c.Name == "investment-tracking-api");
+
+                if (apiContainer.State == ServiceRunningState.Running)
+                {
+                    try
+                    {
+                        var response = await Client.GetAsync("swagger/index.html");
+                        if (response.IsSuccessStatusCode) return;
+                    }
+                    catch (Exception)
+                    {
+                        // ignored
+                    }
+                }
+
+                Thread.Sleep(delay);
+            }
+
+            throw new Exception("Docker Compose configuration failed to start.");
+        }
+    }
+}
